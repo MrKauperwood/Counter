d805b5610009f55152831b127b4e34227728c994
Aleksei Bondarenko <LexaBondRec@gmail.com>
2023-12-06 00:20:49 +0200

    Changes after refactoring

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,60 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var UITextForCounter: UILabel!
     
-    @IBOutlet weak var UICounterValue: UILabel!
-    
-    @IBOutlet weak var UIPlusButton: UIButton!
-    
-    @IBOutlet weak var UIMinusButton: UIButton!
-    
-    @IBOutlet weak var UIClearButton: UIButton!
-    
-    @IBOutlet weak var UIHistoryTitle: UITextView!
-    
-    @IBOutlet weak var UILogs: UITextView!
-    
-    private static let dateFormatter : DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yy HH:mm:ss"
-        return dateFormatter
-    }()
-    
-    private func logAction(_ entry : Logs) {
-        UILogs.text = entry.fullMessage + UILogs.text
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    @IBAction func increaseCounter(_ sender: Any) {
-        if let currectValue = Int(UICounterValue.text ?? "0") {
-            UICounterValue.text = String(currectValue + 1)
-            logAction(Logs.increment)
-
-        }
-    }
-    
-    @IBAction func decreaseCounter(_ sender: Any) {
-        if let currectValue = Int(UICounterValue.text ?? "0") {
-            if currectValue > 0 {
-                UICounterValue.text = String(currectValue - 1)
-                logAction(Logs.decrement)
-            } else {
-                logAction(Logs.decrementBelowZero)
-            }
-        }
-    }
-    
-    @IBAction func clearCounterValue(_ sender: Any) {
-        UICounterValue.text = "0"
-        logAction(Logs.reset)
-    }
-    
-    enum Logs {
+    private enum Logs {
         case increment
         case decrement
         case reset
@@ -79,10 +27,60 @@ class ViewController: UIViewController {
                 return "[\(date)]: значение сброшено\n"
             case .decrementBelowZero:
                 return "[\(date)]: попытка уменьшить значение счётчика ниже 0\n"
-                
             }
+        }
+    }
+    
+    @IBOutlet private weak var titleCounterLabel: UILabel!
+    
+    @IBOutlet private weak var counterValueLabel: UILabel!
+    
+    @IBOutlet private weak var plusButton: UIButton!
+    
+    @IBOutlet private weak var minusButton: UIButton!
+    
+    @IBOutlet private weak var clearButton: UIButton!
+    
+    @IBOutlet private weak var titleForHistoryTextView: UITextView!
+    
+    @IBOutlet private weak var historyTextView: UITextView!
+    
+    private static let dateFormatter : DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yy HH:mm:ss"
+        return dateFormatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func logAction(_ entry : Logs) {
+        historyTextView.text = entry.fullMessage + historyTextView.text
+    }
+    
+    @IBAction private func increaseCounter(_ sender: Any) {
+        if let correctValue = Int(counterValueLabel.text ?? "0") {
+            counterValueLabel.text = String(correctValue + 1)
+            logAction(Logs.increment)
             
         }
+    }
+    
+    @IBAction private func decreaseCounter(_ sender: Any) {
+        if let correctValue = Int(counterValueLabel.text ?? "0") {
+            if correctValue > 0 {
+                counterValueLabel.text = String(correctValue - 1)
+                logAction(Logs.decrement)
+            } else {
+                logAction(Logs.decrementBelowZero)
+            }
+        }
+    }
+    
+    @IBAction private func clearCounterValue(_ sender: Any) {
+        counterValueLabel.text = "0"
+        logAction(Logs.reset)
     }
     
 }
